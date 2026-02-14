832f3528ea88b4951f0ff070f5a066d05cfae646
Abhinandan Kumar <abhinandanp635@gmail.com>
2026-02-14 18:36:45 +0530

    Day5:Sum of all element in array

---
+++ Day05_Araay/sumarray.java
@@ -0,0 +1,24 @@
+//package Day05_Araay;
+
+import java.util.Scanner;
+
+public class sumarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size :");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the array elements :");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int sum = 0;
+        for(int i = 0; i< n; i++){
+            sum = sum + arr[i];
+        }
+        System.out.println("Sum of the array is : " + sum);
+         
+
+    }
+}
