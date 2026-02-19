6274c4cbf98df9681d3dcbed252d8be4521801b9
Abhinandan Kumar <abhinandanp635@gmail.com>
2026-02-19 22:14:26 +0530

    Day:7 minimum element in  array

---
+++ Day07_Arrays/MinimumElement.java
@@ -0,0 +1,16 @@
+//package Day07_Arrays;
+
+public class MinimumElement {
+    public static void main(String[] args) {
+        int[] arr = {4, 7, 2, 9, 5};
+        int min = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Minimum element is: " + min);
+    }
+}
