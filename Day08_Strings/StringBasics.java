36fe6808c2a42a63ede9cbdd9a91d9e86b92d7be
Abhinandan Kumar <abhinandanp635@gmail.com>
2026-02-20 11:24:15 +0530

    Day08:String basic

---
+++ Day08_Strings/StringBasics.java
@@ -0,0 +1,11 @@
+// package Day08_Strings;
+
+import java.util.Scanner;
+
+public class StringBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s);
+    }
+}
