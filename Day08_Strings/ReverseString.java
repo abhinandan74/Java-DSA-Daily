755d54d99582cecc3e294b77afca7b946edd2707
Abhinandan Kumar <abhinandanp635@gmail.com>
2026-02-24 19:51:05 +0530

    Compare to string method

--- Day08_Strings/ReverseString.java
+++ Day08_Strings/ReverseString.java
@@ -5,7 +5,7 @@ public class ReverseString {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--)
+        for (int i = s.length() - 1; i >= 0; i--) 
             rev += s.charAt(i);
         System.out.println(rev);
     }
---
+++ Day09_Strings/ComareToStringMethod.java
@@ -0,0 +1,9 @@
+//package Day09_Strings;
+
+public class ComareToStringMethod {
+    public static void main(String[] args) {
+        String a = "anmol";
+        String b = "ankush";
+        System.out.println(a.compareTo(b));
+    }
+}
