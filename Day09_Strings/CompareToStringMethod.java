dfccf3ff05ec7123542e090b7d61d2c45d551748
Abhinandan Kumar <abhinandanp635@gmail.com>
2026-02-26 15:04:15 +0530

    concat function in java string

--- Day09_Strings/CompareToStringMethod.java
+++ Day09_Strings/CompareToStringMethod.java
@@ -1,9 +1,10 @@
 //package Day09_Strings;
 
-public class ComareToStringMethod {
+public class CompareToStringMethod {
     public static void main(String[] args) {
         String a = "anmol";
         String b = "ankush";
         System.out.println(a.compareTo(b));
+        //System.out.println(a.compareTo(b));
     }
 }
---
+++ Day09_Strings/ConcatString.java
@@ -0,0 +1,8 @@
+public class ConcatString {
+    public static void main(String[] args) {
+        String a = "Anmol";
+        String b = "Ankush";
+        //a = a+b;
+        System.out.println(a.concat(b));
+    }
+}
