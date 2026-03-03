4be9006b3efd290dfbff9535018f705ff8b6367b
Abhinandan Kumar <abhinandanp635@gmail.com>
2026-03-03 22:00:25 +0530

    internig problem in java

--- Day09_Strings/ConcatString.java
+++ Day09_Strings/ConcatString.java
@@ -1,5 +1,4 @@
-public class ConcatString {
-    public static void main(String[] args) {
+public class ConcatString {     public static void main(String[] args) {
         String a = "Anmol";
         String b = "Ankush";
         //a = a+b;
---
+++ Day10_Strings/Interning.java
@@ -0,0 +1,10 @@
+public class Interning {
+    public static void main(String[] args) {
+        String s = "Hello"; // -> Heylo // index 2
+        // String x = "Hello";
+        // x = "mello";
+        // System.out.println(x);
+        s = s.substring(0,2) + 'y' + s.substring(3);
+        System.out.println(s); 
+    }
+}
