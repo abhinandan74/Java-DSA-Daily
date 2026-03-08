af01c30d0766b629a2e273a5391aef94a603682c
Abhinandan Kumar <abhinandanp635@gmail.com>
2026-03-08 16:15:38 +0530

    Substring problem in java

---
+++ Day11_Strings/SubStringsOfString.java
@@ -0,0 +1,13 @@
+//public package Day11_Strings;
+
+class SubStringsOfString {
+    public static void main(String[] args) {
+        String s = "gopi";
+        for(int i=0;i<s.length();i++){
+            for(int j=i; j<s.length();j++){
+                System.out.print(s.substring(i,j+1)+" "); // i to j
+            }
+            System.out.println();
+        }
+    }   
+}
